596afcab6e8474dd5494eb09af3cad0371a2cccd
Mabroor Ahmed <ma942@student.le.ac.uk>
2023-05-23 14:45:11 +0100

    Using usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
+import { useState } from "react"
 import "./styles.css"
 export default function App(){
+  const [newItem,setnewItem] = useState("")
+  const [todos,setTodos] = useState([])
+  
+  function handleSubmit(e){
+    e.preventDefault()
+
+    setTodos((currentTodos)=>{
+      return [
+        ...currentTodos,
+        {id: crypto.randomUUID(), title: newItem , completed:false},
+
+      ]
+    }) 
+   }
   return (
   <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
+
       <div className="form-row">
         <label>New Item</label>
-        <input type="text" id="item"/>
+        <input value ={newItem}
+         onChange={e=> setnewItem(e.target.value)}
+        type="text"
+        id="item"/>
 
       </div>
     <button className="btn"> Add</button> 
     </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
-      <li>
-        <label>
-          <input type ="checkbox"/>
-          Item 1
-        </label>
-      </li>
-      <button className="btn btn-danger">Delete</button>
-    </ul>
+      {todos.map(todo => {
+        return (
+            <li key={todo.id}>
+              <label>
+                <input type ="checkbox" checked = {todo.completed}/>
+                {todo.title}
+              </label>
+            <button className="btn btn-danger">Delete</button>
+            </li>
+          )
+        })}  
+      </ul>
     </>
     )
 }
