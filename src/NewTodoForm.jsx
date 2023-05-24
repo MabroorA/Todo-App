4e72b06bda14480acf59b902b9a84171a4c33942
Mabroor Ahmed <ma942@student.le.ac.uk>
2023-05-24 15:28:34 +0100

    TodoList Component

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,30 +1,31 @@
 import { useState } from "react"
-export function NewTodoForm(){
+
+
+
+
+export function NewTodoForm({onSubmit}){
+    
     const [newItem,setnewItem] = useState("")
+
     function handleSubmit(e){
         e.preventDefault()
-    
-        // setTodos((currentTodos)=>{
-        //   return [
-        //     ...currentTodos,
-        //     {id: crypto.randomUUID(), title: newItem , completed:false},
-    
-        //   ]
-        // }) 
+        if (newItem === "")return 
+
+        onSubmit(newItem)
+
         setnewItem("")
        }
 
     return (
         <form onSubmit={handleSubmit} className="new-item-form">
+            <div className="form-row">
+                <label>New Item</label>
+                <input value ={newItem}
+                onChange={e=> setnewItem(e.target.value)}
+                type="text"
+                id="item"/>
 
-        <div className="form-row">
-            <label>New Item</label>
-            <input value ={newItem}
-            onChange={e=> setnewItem(e.target.value)}
-            type="text"
-            id="item"/>
-
-        </div>
+            </div>
         <button className="btn"> Add</button> 
     </form>
     )
